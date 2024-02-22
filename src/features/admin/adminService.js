52f27587f6621fcb033829b6d178d8987a1aba3f
Kerem Can KURT <127202679+keremcankurt@users.noreply.github.com>
2024-02-22 13:11:19 +0300

    Update adminService.js

--- src/features/admin/adminService.js
+++ src/features/admin/adminService.js
@@ -1,5 +1,5 @@
 import {put} from '../request';
-const BASE_URL = "http://localhost:4000/api/admin";
+const BASE_URL = "http://ec2-16-171-159-119.eu-north-1.compute.amazonaws.com:4000/api/admin";
 
 const rejectSeller = id => put(`${BASE_URL}/rejectseller?id=${id}`);
 const confirmSeller = data => put(`${BASE_URL}/confirmseller?id=${data.id}&company=${data.company}`);
